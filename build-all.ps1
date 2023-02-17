5b0228cb947b8a379dd4f9d075e4d141ac523e08
RaphaelTarita <raphael.tarita@lean-coders.at>
2023-02-17 15:08:52 +0100

    add vue storefront (WIP)

--- build-all.ps1
+++ build-all.ps1
@@ -1,3 +1,4 @@
 Set-Variable HOST_IP (Get-NetIPAddress -AddressFamily IPv4 -AddressState Preferred | Where-Object -FilterScript { ($_.InterfaceAlias -Notlike "*WSL*") -and ($_.InterfaceAlias -Notlike "*Loopback*") }).IPAddress
 docker build vendure-server -t vendure-server-docker
 docker build vendure-storefront-remix -t vendure-storefront-remix-docker --build-arg VENDURE_SERVER_HOST=$HOST_IP
+docker build vendure-storefront-vue -t vendure-storefront-vue-docker --build-arg VENDURE_SERVER_HOST=$HOST_IP
--- build-all.sh
+++ build-all.sh
@@ -1,3 +1,4 @@
 HOST_IP=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 docker build vendure-server -t vendure-server-docker
 docker build vendure-storefront-remix -t vendure-storefront-remix-docker --build-arg VENDURE_SERVER_HOST="$HOST_IP"
+docker build vendure-storefront-vue -t vendure-storefront-vue-docker --build-arg VENDURE_SERVER_HOST="$HOST_IP"
